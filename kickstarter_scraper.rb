7c3dc92899afc723b9c57130243295fd8bd9749c
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-06-20 16:25:06 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -12,10 +12,10 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
 
   kickstarter = Nokogiri::HTML(html)
-  
+
   projects = {}
- 
-  # Iterate through the projects
+
+  # iterate through the projects
   kickstarter.css("li.project.grid_4").each do |project|
     projects[project] = {}
   end
