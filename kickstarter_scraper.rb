679288e77e9f8594082a644ac0c139273594a349
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-06-20 15:09:20 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,6 +10,6 @@ def create_project_hash
   # title: project.css("h2.bbcard_name strong a").text
   # image link: project.css("div.project-thumbnail a img").attribute("src").value
   # description: project.css("p.bbcard_blurb").text
-  # location: project.css("ul.project-meta span-location.name").text
+  # location: project.css("ul.project-meta").text
   binding.pry
 end
